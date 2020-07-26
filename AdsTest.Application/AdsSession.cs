35bd3de1631a348e35dfc9e88778d7847b0fd910
Omid Talebi <omid.talebi@hotmail.com>
2020-07-26 09:20:04 +0430

    add adsSession

---
+++ AdsTest.Application/AdsSession.cs
@@ -0,0 +1,32 @@
+﻿using Abp.Dependency;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+
+namespace AdsTest
+{
+    public class AdsSession: ITransientDependency
+    {
+        public string SubsystemId
+        {
+            get
+            {
+                var claimsPrincipal = Thread.CurrentPrincipal as ClaimsPrincipal;
+                if (claimsPrincipal == null)
+                {
+                    return null;
+                }
+
+
+                var emailClaim = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == "SubsystemId");
+                if (emailClaim == null || string.IsNullOrEmpty(emailClaim.Value))
+                {
+                    return null;
+                }
+
+                return emailClaim.Value;
+            }
+        }
+
+    }
+}
