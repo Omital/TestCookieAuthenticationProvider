f7d4422f10d1c86feba55767114c99aed4b153cd
Omid Talebi <omid.talebi@hotmail.com>
2020-07-27 08:19:07 +0430

    override CreateIdentityAsync to add custom claim

--- AdsTest.Core/Authorization/Users/UserManager.cs
+++ AdsTest.Core/Authorization/Users/UserManager.cs
@@ -3,16 +3,21 @@ using Abp.Authorization.Users;
 using Abp.Configuration;
 using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
+using Abp.Extensions;
 using Abp.IdentityFramework;
 using Abp.Localization;
 using Abp.Organizations;
 using Abp.Runtime.Caching;
 using AdsTest.Authorization.Roles;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace AdsTest.Authorization.Users
 {
     public class UserManager : AbpUserManager<Role, User>
     {
+        public AdsSession AdsSession { get; set; }
+
         public UserManager(
             UserStore userStore,
             RoleManager roleManager,
@@ -40,6 +45,15 @@ namespace AdsTest.Authorization.Users
                   settingManager,
                   userTokenProviderAccessor)
         {
+        }
+
+        public async override Task<ClaimsIdentity> CreateIdentityAsync(User user, string authenticationType)
+        {
+            var idntities = await base.CreateIdentityAsync(user, authenticationType);
+            if (AdsSession.SubsystemId.IsNullOrEmpty() == false)
+                idntities.AddClaim(new Claim("SubsystemId", AdsSession.SubsystemId));
+
+            return idntities;
         }
     }
 }
